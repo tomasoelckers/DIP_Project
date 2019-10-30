00754ea043f4deaf801405aff162076b5d73908d
toelckers <toelckers@bitbucket.org>
2019-10-29 22:29:30 -0300

    colorChannel class fixed

--- colorChannel.py
+++ colorChannel.py
@@ -1,8 +1,6 @@
 # Transform RGB to HSV, L*a*b* and CMYK.
 # Divide in 10 channel colour.
 
-from skimage.color import rgb2hsv
-from skimage.color import rgb2lab
 from PIL import Image
 import numpy as np
 import cv2
@@ -13,11 +11,12 @@ class ColourTransformation:
         im = Image.fromarray(image)
         im = im.convert('CMYK')
 
-        self.image = image
-        self.HSV = rgb2hsv(image)
-        self.Lab = rgb2lab(image)
+        self.RGB = image
+        self.HSV = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        self.Lab = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
         self.CMYK = np.array(im)
 
+        self.R, self.G, self.B = cv2.split(self.RGB)
         self.H, self.S, self.V = cv2.split(self.HSV)
         self.L, self.a, self.b = cv2.split(self.Lab)
         self.C, self.M, self.Y, self.K = cv2.split(self.CMYK)
