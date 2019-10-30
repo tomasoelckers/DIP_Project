1f807b2c687369ace7e213311ad004a02d135154
toelckers <toelckers@bitbucket.org>
2019-10-29 23:10:52 -0300

    ...

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from colorChannel import ColourTransformation
+
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import cv2
-from colorChannel import ColourTransformation
+
 
 # Load image in BGR.
 imagePath = 'Images/DSC_0010.jpg'
